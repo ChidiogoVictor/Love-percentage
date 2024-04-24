a178c7820d53b2c1908ca2d92f85fc53fc6a08d0
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-04-24 11:01:05 +0100

    Initial Repository

---
+++ index.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your name?");    
+var SecondName = prompt("What is your friend name?");
+var lovePer = Math.random();
+lovePer = lovePer * 101;
+lovePer = Math.floor(lovePer);
+alert(firstName + " and " + SecondName + " Love score is " + lovePer + "%.");
