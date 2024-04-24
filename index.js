9fe5faddea593ff7d9d985d3f83643de1ab2390d
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-04-24 11:59:01 +0100

    added else if statement

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ lovePer = Math.floor(lovePer);
 
 if(lovePer >= 70){
     alert(firstName + " and " + SecondName + " Love score is " + lovePer + "%, You are best lovers ever!💖");
+} else if (lovePer > 50 && lovePer < 70){
+    alert(firstName + " and " + SecondName + " Love score is " + lovePer + "%, You are good lovers!");
 } else{
     alert(firstName + " and " + SecondName + " Love score is " + lovePer + "%.");
 }
