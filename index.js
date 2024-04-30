17f75a3977dd4c47840f7668282829b603ea6cf1
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-05-01 00:50:06 +0100

    updated the code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var firstName = prompt("What is your name?");    
 var SecondName = prompt("What is your friend name?");
 var lovePer = Math.random();
-lovePer = lovePer * 101;
-lovePer = Math.floor(lovePer);
+lovePer = lovePer * 100;
+lovePer = Math.floor(lovePer) + 1;
 
 if(lovePer >= 70){
     alert(firstName + " and " + SecondName + " Love score is " + lovePer + "%, You are best lovers ever!💖");
